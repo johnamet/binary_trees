095d7fb158e3c26d1ec6caac8220d371f1c69bca
Akwolu Innocent <Innocentakwolu1@gmail.com>
2024-01-04 07:59:29 -0800

    Corrected

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
+
 /**
- * function that measures the depth of a node
+ * binary_tree_depth - function that measures the depth of a node
  * in a binary tree
  * @tree: pointer to the node to measure the depth
- * return: Return depth on Success
+ * Return: Return depth on Success
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
