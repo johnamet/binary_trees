b3791354d40aad214b5b7fd2b1160003c57025aa
johnny <johnametepeagboku@live.com>
2024-01-04 14:27:33 +0000

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,9 +11,27 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	int left_height = 0;
+	binary_tree_t *left = tree->left;
+	binary_tree_t *right = tree->right;
+	int right_height = 0;
+
+	if (left != NULL)
+	{
+		left_height++;
+		left = left->left;
+	}
+
+	if (right != NULL)
+	{
+		right_height++;
+		right = right->right;
+	}
+
+	right_height += binary_tree_height(tree->right);
+	left_height += binary_tree_height(tree->left);
+
+	return ((left_height > right_height ? left_height : right_height));
 }
 
