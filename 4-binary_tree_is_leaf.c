39dd58bedb2fd122413b8890faaca0c577bc58f3
johnny <johnametepeagboku@live.com>
2024-01-04 15:27:44 +0000

    Fixup: Task 4- Betty coding style

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_is_leaf - function that checks if a node is a leaf
  * @node:  pointer to the node to check
- * return: Return 0 on failure
+ * Return: Return 0 on failure
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
