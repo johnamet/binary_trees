ce1f9f903194d9133102d8731e323bba65560934
johnny <johnametepeagboku@live.com>
2024-01-04 00:01:45 +0000

    Fixup Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,6 +14,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int left_height = binary_tree_height(tree->left);
 	int right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height ) +1);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
