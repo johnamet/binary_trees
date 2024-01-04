d41270ac952d291869ba741a40aa62d45c805fbc
Akwolu Innocent <Innocentakwolu1@gmail.com>
2024-01-04 06:04:25 -0800

    Binary Trees

--- binary_trees.h
+++ binary_trees.h
@@ -30,4 +30,17 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+
 #endif
