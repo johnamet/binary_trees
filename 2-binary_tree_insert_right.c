820c86206c08f1b1f72a9b8e4e562eb4b3482596
johnny <johnametepeagboku@live.com>
2024-01-04 15:33:11 +0000

    Fixup

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,7 +4,7 @@
  * right-child of another node
  * @parent: Pointer to the parent node
  * @value: the value to store in the new node
- * return: Return NULL on failure
+ * Return: Return NULL on failure
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
