1e36458c38d6a77e8b6060c67cceecd553bd5226
johnny <johnametepeagboku@live.com>
2024-01-05 16:11:11 +0000

    Fixup: Tsk 100: Coding style

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
- * @first - Pointer to the first node
- * @second - Pointer to the second node
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
  *
  * Return: Pointer to the lowest common ancestor otherwise NULL
  */
@@ -10,6 +10,7 @@ binary_tree_t *binary_trees_ancestor(const  binary_tree_t *first,
 		const binary_tree_t *second)
 {
 	const binary_tree_t *ancestor;
+
 	if (first == NULL || second == NULL ||
 			(first->parent == NULL && second->parent == NULL))
 		return (NULL);
@@ -24,8 +25,8 @@ binary_tree_t *binary_trees_ancestor(const  binary_tree_t *first,
 		{
 			if (ancestor == temp)
 				return ((binary_tree_t *)ancestor);
-			else
-				temp = temp->parent;
+
+			temp = temp->parent;
 		}
 
 		ancestor = ancestor->parent;
